c4763b409015db590aca97bd90e7215507b5ae69
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-01-31 21:53:01 -0800

    home page now gets all pets from database

--- app.py
+++ app.py
@@ -2,17 +2,22 @@
 
 from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-
+from models import connect_db, db, Pet
 
 app = Flask(__name__)
 
 app.config.update(
-    SQLALCHEMY_DATABASE_URI="postgresql:///pet_adoption",
+    SQLALCHEMY_DATABASE_URI="postgresql:///adoption",
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    SQLALCHEMY_ECHO=True,
+    # SQLALCHEMY_ECHO=True,
     SECRET_KEY="mensajesecreto",
     DEBUG_TB_INTERCEPT_REDIRECTS=False
 )
+
+connect_db(app)
+# db.drop_all()
+# db.create_all()
+
 # Debug Extension on
 debug = DebugToolbarExtension(app)
 
@@ -20,4 +25,5 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def home():
     """ Home page """
-    return render_template("index.html")
+    pets = Pet.get_all_pets()
+    return render_template("index.html", pets=pets)
