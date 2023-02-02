4117026e7879d4399f8b3486658dd1ffc3293312
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-02 13:43:27 -0800

    new model EditPetForm added, updated additional fields

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField
-from wtforms.validators import InputRequired, Optional, NumberRange
+from wtforms import StringField, IntegerField, SelectField, BooleanField
+from wtforms.validators import InputRequired, Optional, NumberRange, URL, Length
 
 
 class AddPetForm(FlaskForm):
@@ -9,11 +9,23 @@ class AddPetForm(FlaskForm):
                        InputRequired(message="Please Enter a Name")])
 
     species = SelectField("Pet Species", validators=[
-                          InputRequired(message="Please Enter a Species")], choices=[("cat", "cat"), ("dog", "dog"), ("porcupine", "porcupine"), ("other", "other")])
+                          InputRequired(message="Please Enter a Species")], choices=[("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine"), ("other", "Other")])
 
-    photo_url = StringField("Photo URL", validators=[Optional()])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
 
     age = IntegerField("Pet age", validators=[
                        Optional(), NumberRange(min=0, max=30, message="Age must be between 0 - 30")])
 
-    notes = StringField("Pet Notes", validators=[Optional()])
+    notes = StringField("Pet Notes", validators=[Optional(),  Length(max=50)])
+
+
+class EditPetForm(FlaskForm):
+    """ Form for editing an existing Pet """
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+
+    age = IntegerField("Pet age", validators=[
+                       Optional(), NumberRange(min=0, max=30, message="Age must be between 0 - 30")])
+
+    notes = StringField("Pet Notes", validators=[Optional(),  Length(max=50)])
+
+    available = BooleanField("Available?")
