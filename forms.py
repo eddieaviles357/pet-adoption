279a8262035e12c6cdc0f5ca5ddc3a982186851c
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-01 19:08:16 -0800

    added a list for species for user to choose from, made some minor changes with messages

--- forms.py
+++ forms.py
@@ -1,14 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import InputRequired, Optional
+from wtforms import StringField, IntegerField, SelectField
+from wtforms.validators import InputRequired, Optional, NumberRange
 
 
 class AddPetForm(FlaskForm):
     """ Form for adding Pet """
     name = StringField("Pet Name", validators=[
-                       InputRequired("Please Enter a Name")])
-    species = StringField("Pet Species", validators=[
-                          InputRequired("Please Enter a Species")])
+                       InputRequired(message="Please Enter a Name")])
+
+    species = SelectField("Pet Species", validators=[
+                          InputRequired(message="Please Enter a Species")], choices=[("cat", "cat"), ("dog", "dog"), ("porcupine", "porcupine"), ("other", "other")])
+
     photo_url = StringField("Photo URL", validators=[Optional()])
-    age = IntegerField("Pet age", validators=[Optional()])
+
+    age = IntegerField("Pet age", validators=[
+                       Optional(), NumberRange(min=0, max=30, message="Age must be between 0 - 30")])
+
     notes = StringField("Pet Notes", validators=[Optional()])
