79dc146d5ec7397a791902b6d5bfefa66e78fd65
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-02 13:42:08 -0800

    pet details route now handles editing of pet and displays details of pet

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 
 from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db, Pet
-from forms import AddPetForm
+from models import connect_db, db, Pet, DEFAULT_IMG
+from forms import AddPetForm, EditPetForm
 app = Flask(__name__)
 
 app.config.update(
@@ -21,6 +21,8 @@ connect_db(app)
 # Debug Extension on
 debug = DebugToolbarExtension(app)
 
+# GET /
+
 
 @app.route('/')
 def home():
@@ -28,17 +30,23 @@ def home():
     pets = Pet.get_all_pets()
     return render_template("index.html", pets=pets)
 
+# GET /add
+# POST /add
+
 
 @app.route("/add", methods=["GET", "POST"])
 def add_pet():
     """ Add pet route """
     form = AddPetForm()
+    # import pdb
+    # pdb.set_trace()
+    species = Pet.get_all_pets_species()
     # if form does not validate or if route is a GET req
     if form.validate_on_submit():
         # get data from client
         name = form.name.data
         species = form.species.data
-        photo_url = form.photo_url.data
+        photo_url = form.photo_url.data or None
         age = form.age.data
         notes = form.notes.data
         # create a Pet
@@ -54,11 +62,24 @@ def add_pet():
     else:
         return render_template("add-pet.html", form=form)
 
+# GET /[pet-id]
+# POST /[pet-id]
 
-@app.route("/<int:pet_id>")
+
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
 def pet_details(pet_id):
-    """ Display details about pet """
-    pet = Pet.query.filter(pet_id)
-    import pdb
-    pdb.set_trace()
-    return render_template("pet-details.html", pet=pet)
+    """ Display details about pet and edit pet details """
+    pet = Pet.query.get_or_404(pet_id)
+    # prepoluate Edit form
+    form = EditPetForm(obj=pet)
+    if form.validate_on_submit():
+        # if user leaves photo emtpy a default will be assigned
+        pet.photo_url = form.photo_url.data or DEFAULT_IMG
+        pet.age = form.age.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        # update pet
+        db.session.commit()
+        flash("Updated successful", "success")
+        return redirect("/")
+    return render_template("pet-details.html", pet=pet, form=form)
